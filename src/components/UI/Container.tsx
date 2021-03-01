f0582aac43bce034acef241c21d68b2eb01d4f1e
Todd Alexander <60899614+toddalex@users.noreply.github.com>
2021-02-28 18:22:37 -0800

    update styles

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -23,6 +23,7 @@ const styles: {
     paddingTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: '5px',
     boxShadow: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px'
   }
 };
